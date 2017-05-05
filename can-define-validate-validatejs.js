9a8eda32b7257c1eb710c94ac2dfa7cfaea7be3d
Juan Orozco <hi@juanorozco.com>
2017-05-05 14:38:22 -0600

    Interim commit, needs rebasing

--- can-define-validate-validatejs.js
+++ can-define-validate-validatejs.js
@@ -5,9 +5,11 @@ var each = require("can-util/js/each/each");
 var assign = require("can-util/js/assign/assign");
 var isEmptyObject = require("can-util/js/is-empty-object/is-empty-object");
 
+// TODO: Move to can-define-validate
 var getMapConstraints = function (Map) {
     var constraints = {};
     each(Map.prototype._define.definitions, function (prop, key) {
+        // TODO: Will need to check for `related` properties here
         if (prop.validate && !isEmptyObject(prop.validate)) {
             constraints[key] = prop.validate;
         }
@@ -41,8 +43,10 @@ var validateMap = function (Map, validator) {
                 assign(mapClone, values);
                 values = mapClone;
             }
+            // TODO: when async, this will return a deferred?
             return validator(values);
         } else {
+            // TODO: when async this could return a deferred?
             return this.errors();
         }
 
@@ -68,18 +72,67 @@ var validateMap = function (Map, validator) {
     define.property(Map.prototype, "_errors", {
         get: function(){
             var errors = validator(this);
-            return errors;
+            return Promise.all(errors);
         }
     }, mapDefinition.dataInitializers, mapDefinition.computedInitializers);
 };
 
 var decorator = function(Map) {
     var constraints = getMapConstraints(Map);
-    var validator = validate.many(constraints);
+
+    // Create validator for each property and its constraints
+    var validations = constraints.map(function(prop){
+      // TODO: By using just the `validate` method, we run into the use case of
+      // validating one property against another.
+      // (ex: validate phone number based on country)...
+
+      // 1) Use `validate` to validate a single property but force user to add
+      // a related property to constraints. This means we need to strip out related
+      // from constraints before sending it to validation library and that users
+      // cannot use validateJS' custom validator API.
+
+      // 2) Continue to use `validate.many` but then `many` will need to detect
+      // promises BEFORE validation occurs (in order to use correct valdiatejs method)
+      // return validate.many(prop);
+      return validate(prop);
+    });
+
+
+    var validator = function (map) {
+      var foundPromise = false;
+
+      // process each validator, look for promises
+      var results = validations.map(function(validate){
+        var res = validate(someValue)
+
+        if(!foundPromise && isPromise(res)) {
+          foundPromise = true;
+        }
+        return res;
+      });
+      return foundPromise ? Promise.all(results) : results;
+    }
 
     validateMap(Map, function (map) {
         var errors = validator(map);
+
+        // TODO: build this method out in can-define-validate or can-validate?
+        // errors = processRelatedErrors(errors);
+
         return formatErrors(errors, 'errors');
+    });
+};
+
+// TODO: Don't need.
+decorator.async = function (Map) {
+    var constraints = getMapConstraints(Map);
+    var validator = validate.async(constraints);
+
+    validateMap(Map, function (map) {
+        // Return compute that updates when async is done?
+        return validator.async(map).then(function (errors) {
+          return formatErrors(errors, 'errors');
+        });
     });
 };
 
