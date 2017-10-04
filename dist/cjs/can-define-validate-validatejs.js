63fb3b35b011d92a39e3d44aa11f015b8a486f93
Juan Orozco <hi@juanorozco.com>
2017-10-04 10:50:23 -0400

    0.1.2

---
+++ dist/cjs/can-define-validate-validatejs.js
@@ -0,0 +1,74 @@
+/*can-define-validate-validatejs@0.1.1#can-define-validate-validatejs*/
+var formatErrors = require('can-validate').formatErrors;
+var validate = require('can-validate-validatejs');
+var define = require('can-define');
+var each = require('can-util/js/each/each');
+var assign = require('can-util/js/assign/assign');
+var isEmptyObject = require('can-util/js/is-empty-object/is-empty-object');
+var getMapConstraints = function (Map) {
+    var constraints = {};
+    each(Map.prototype._define.definitions, function (prop, key) {
+        if (prop.validate && !isEmptyObject(prop.validate)) {
+            constraints[key] = prop.validate;
+        }
+    });
+    return constraints;
+};
+var validateMap = function (Map, validator) {
+    var mapDefinition = Map.prototype._define;
+    Map.prototype.testSet = function () {
+        var values = {};
+        var useNewObject = false;
+        if (arguments.length) {
+            if (typeof arguments[0] === 'object' && Boolean(arguments[0])) {
+                values = arguments[0];
+                useNewObject = Boolean(arguments[1]);
+            }
+            if (typeof arguments[0] === 'string') {
+                values[arguments[0]] = arguments[1];
+            }
+            if (useNewObject) {
+                values = new Map(values);
+            } else {
+                var mapClone = this.serialize();
+                assign(mapClone, values);
+                values = mapClone;
+            }
+            return validator(values);
+        } else {
+            return this.errors();
+        }
+    };
+    Map.prototype.errors = function () {
+        var _errors = this._errors;
+        var errors;
+        if (arguments.length) {
+            var errorsObj = formatErrors(_errors, 'errors-object');
+            errors = [];
+            each(arguments, function (key) {
+                [].push.apply(errors, errorsObj ? errorsObj[key] : []);
+            });
+            errors = errors.length > 0 ? errors : undefined;
+        } else {
+            errors = _errors;
+        }
+        return errors;
+    };
+    define.property(Map.prototype, '_errors', {
+        get: function () {
+            var errors = validator(this);
+            return errors;
+        }
+    }, mapDefinition.dataInitializers, mapDefinition.computedInitializers);
+};
+var decorator = function (Map) {
+    var constraints = getMapConstraints(Map);
+    var validator = validate.many(constraints);
+    validateMap(Map, function (map) {
+        var errors = validator(map);
+        return formatErrors(errors, 'errors');
+    });
+};
+decorator.validatejs = validate.validatejs;
+module.exports = decorator;
+//# sourceMappingURL=can-define-validate-validatejs.js.map
